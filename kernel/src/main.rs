374bd6a7bf67c2264b97970ade30da57dbfa9a52
Franciszek Łopuszański <franopusz2006@gmail.com>
2024-07-07 00:06:42 +0200

    Add basic framebuffer (not arch agnostic)

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,11 +1,27 @@
 #![no_std]
 #![no_main]
 
+#[macro_use]
+extern crate lazy_static;
+
 use core::panic::PanicInfo;
+use limine::request::FramebufferRequest;
 
 mod arch;
+mod framebuffer;
+
+static FRAMEBUFFER: FramebufferRequest = FramebufferRequest::new();
 
 pub fn main() -> ! {
+    if let Some(framebuffer_response) = FRAMEBUFFER.get_response() {
+        let framebuffer = match framebuffer_response.framebuffers().next() {
+            Some(i) => i,
+            None => arch::halt_loop(),
+        };
+
+        framebuffer::init(&framebuffer);
+    }
+
     arch::halt_loop();
 }
 
